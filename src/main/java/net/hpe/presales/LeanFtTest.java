027b932c871333dd661f7d9ad5e2093f90f7f6e8
Dave Flynn <flynn@hpe.com>
2017-03-23 16:23:48 -0500

    restored LeanFtTest class that was deleted by accident

---
+++ src/main/java/net/hpe/presales/LeanFtTest.java
@@ -0,0 +1,52 @@
+package net.hpe.presales;
+
+import static org.junit.Assert.*;
+
+import com.hpe.alm.octane.OctaneCucumber;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.hp.lft.sdk.*;
+import com.hp.lft.verifications.*;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+import unittesting.*;
+
+//@RunWith(Cucumber.class)
+@RunWith(OctaneCucumber.class)
+//The below was done to insure a good run no matter what other feature files may be added.
+@CucumberOptions(plugin = {"junit:leanftResults.xml"}, features="classpath:advantage.feature")//the feature to execute
+public class LeanFtTest extends UnitTestClassBase {
+
+	public LeanFtTest() {
+		//Change this constructor to private if you supply your own public constructor
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		instance = new LeanFtTest();
+		globalSetup(LeanFtTest.class);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		globalTearDown();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test1() throws GeneralLeanFtException {
+
+	}
+
+}
